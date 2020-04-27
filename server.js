da36ba8c46275b185eab6e396db5383794ae5c8a
Zayd Sarraj <zayd.sarraj@gmail.com>
2020-04-27 19:42:41 -0400

    created deployment build

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/gsn'; 
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://GameAdmin:blastoise1@ds259001.mlab.com:59001/heroku_btvxcx88'; 
 
 mongoose.Promise = global.Promise;
 
