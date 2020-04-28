7a9bff60d1b429f91b6f39c3ac2899da51877997
Zayd Sarraj <zayd.sarraj@gmail.com>
2020-04-28 01:46:17 -0400

    another attempt

--- client/src/components/UserCard/index.js
+++ client/src/components/UserCard/index.js
@@ -7,29 +7,31 @@ import UserInfoCard from '../UserInfoCard';
 
 
 const UserCard = (props) => {
-  if (!props.value.userLoggedIn) {
+  if (!props.value.username) {
     return (<Card className="mx-1 mt-1 shadow">
-      <CardImg top className="border rounded shadow" width="100%" src={props.value.avatar} alt="Profile Picture" />
+      <CardImg top className="border rounded shadow" width="100%" src={props.value.userProfile.avatar} alt="Profile Picture" />
       <CardBody className='text-center border rounded shadow my-2 mx-2'>
-        <CardTitle className="textshadows"><b>{props.value.username}</b></CardTitle>
-        <CardSubtitle>-{props.value.platforms.map(platform => { return <span>{platform} - </span> })}</CardSubtitle>
-        <CardText>{props.value.bio}</CardText>
+        <CardTitle className="textshadows"><b>{props.value.userProfile.username}</b></CardTitle>
+        <CardSubtitle>-{props.value.userProfile.platforms.map((platform, index) => { return <span key={index}>{platform} - </span> })}
+        </CardSubtitle>
+        <CardText>{props.value.userProfile.bio}</CardText>
       </CardBody>
       <UserInfoCard value={props.value} />
     </Card>)
   } else {
     return (<Card className="mx-1 mt-1 shadow">
-      <CardImg top className="border rounded shadow" width="100%" src={props.value.avatar} alt="Profile Picture" />
+      <CardImg top className="border rounded shadow" width="100%" src={props.value.userProfile.avatar} alt="Profile Picture" />
       <CardBody className='text-center border rounded shadow my-2 mx-2'>
-        <CardTitle className="textshadows"><b>{props.value.username}</b></CardTitle>
-        <CardSubtitle>-{props.value.platforms.map(platform => { return <span>{platform} - </span> })}</CardSubtitle>
-        <CardText>{props.value.bio}</CardText>
+        <CardTitle className="textshadows"><b>{props.value.userProfile.username}</b></CardTitle>
+        <CardSubtitle>-{props.value.userProfile.platforms.map((platform, index) => { return <span key={index}>{platform} - </span> })}
+        </CardSubtitle>
+        <CardText>{props.value.userProfile.bio}</CardText>
       </CardBody>
       <UserInfoCard value={props.value} />
-      {props.value.username !== props.value.username ?
+      {props.value.userProfile.username !== props.value.username ?
         <Container className="py-2 text-center">
           <Button className="mb-2 mx-auto border rounded shadow" color="transparent"
-            name={props.value.username} onClick={event => props.addFriend(event)}>Add</Button>
+            name={props.value.userProfile.username} onClick={event => props.addFriend(event)}>Add</Button>
         </Container> : null}
     </Card>)
   }
