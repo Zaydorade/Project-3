e010623d0272e6dea847cddea8df077b747e2005
Zayd Sarraj <zayd.sarraj@gmail.com>
2020-04-27 20:47:18 -0400

    trying to fix api routes

--- client/src/utils/UserAPI.js
+++ client/src/utils/UserAPI.js
@@ -2,39 +2,39 @@ import axios from 'axios';
 
 export default {
 	getUsers: () => {
-		return axios.get('/api/users');
+		return axios.get('api/users');
     },
     getUser: () => {
-        return axios.get('/api/users/user')
+        return axios.get('api/users/user')
     },
     getUserProfile: data => {
-        return axios.get(`../api/users/${data.username}`)
+        return axios.get(`api/users/${data.username}`)
     },
     createUser: data => {
-        return axios.post('/api/users/', data)
+        return axios.post('api/users/', data)
     },
     deleteUser: data => {
-        return axios.post('/api/auth/delete', data)
+        return axios.post('api/auth/delete', data)
     },
 	getSteam: data => {
-        return axios.post('./api/steam', data)
+        return axios.post('api/steam', data)
     },
     updateSteamAvatar: () => {
-        return axios.get(`../api/steam/avatar`)
+        return axios.get(`api/steam/avatar`)
     },
     updateUser: data => {
-        return axios.post(`./api/users/${data.username}`, data)
+        return axios.post(`api/users/${data.username}`, data)
     },
     updateUserfromProfile: data => {
-        return axios.post(`../api/users/${data.username}`, data)
+        return axios.post(`api/users/${data.username}`, data)
     },
     updateAvatar: () => {
-        return axios.get('./api/avatar')
+        return axios.get('api/avatar')
     },
     login: data => {
-        return axios.post('./api/auth/login', data)
+        return axios.post('api/auth/login', data)
     },
     logout: () => {
-        return axios.get('./api/auth/logout')
+        return axios.get('api/auth/logout')
     }
 };
---
+++ routes/api/steam.js
@@ -0,0 +1,49 @@
+const axios = require('axios');
+const router = require('express').Router();
+const db = require('../../models');
+const mongoose = require('mongoose');
+const download = require('image-downloader')
+
+module.exports = router;
+
+router.route('/').get((req, res) => {
+    axios.get('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=BD54D1AE4184BBA8B7ECF21F6094F4CE&steamids=76561198058770552')
+        .then((response) => res.json(response.data.response.players)).catch((error) => res.json(error));
+
+});
+
+router.route('/games').get((req, res) => {
+    axios.get('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=BD54D1AE4184BBA8B7ECF21F6094F4CE&steamid=76561198058770552&format=json')
+        .then((response) => res.json(response.data.response)).catch((error) => res.json(error));
+
+});
+
+
+router.route('/').post((req, res) => {
+    let userSteamID = req.body.steamID
+    let APIkey = 'BD54D1AE4184BBA8B7ECF21F6094F4CE'
+    let steamAPIurl = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${APIkey}&steamids=${userSteamID}`
+    console.log(req.body.steamID)
+    axios.get(steamAPIurl)
+        .then((response) =>
+            db.Users.updateOne({ username: req.body.username }, { steamInfo: response.data.response.players[0] })
+                .then(user => res.json({'success': 'true'}))
+                .catch(err => res.json(err)))
+        .catch((error) => res.json(error));
+});
+
+// router.route('/avatar').get((req, res) => {
+//     if (req.user.steamInfo.avatarfull) {
+//     const tempOptions = {
+//         url: req.user.steamInfo.avatarfull,
+//         dest: 'client/public/steamThumbs/' 
+//     }
+//     download.image(tempOptions)
+//     .then(({filename, image}) => {
+//         console.log({'saved to': filename})
+//     })
+//     .catch(err => console.log(err))
+//     console.log(tempOptions)
+//     res.json({"success" : "maybe"})}
+//     else res.json({"success": "false"})
+// })
