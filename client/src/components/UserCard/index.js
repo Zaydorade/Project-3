508b94f05761dcf82d6e04e8c096580ec991aa58
Zayd Sarraj <zayd.sarraj@gmail.com>
2020-04-28 00:03:30 -0400

    removed add button from profiles when user is not logged in

--- client/src/components/UserCard/index.js
+++ client/src/components/UserCard/index.js
@@ -7,9 +7,8 @@ import UserInfoCard from '../UserInfoCard';
 
 
 const UserCard = (props) => {
-  return (
-
-    <Card className="mx-1 mt-1 shadow">
+  if (!props.value.username) {
+    return (<Card className="mx-1 mt-1 shadow">
       <CardImg top className="border rounded shadow" width="100%" src={props.value.userProfile.avatar} alt="Profile Picture" />
       <CardBody className='text-center border rounded shadow my-2 mx-2'>
         <CardTitle><b>{props.value.userProfile.username}</b></CardTitle>
@@ -17,15 +16,26 @@ const UserCard = (props) => {
         </CardSubtitle>
         <CardText>{props.value.userProfile.bio}</CardText>
       </CardBody>
-      <UserInfoCard value={props.value}/>
-      {props.value.userProfile.username !== props.value.username ? 
-      <Container className="py-2 text-center">
-        <Button className="mb-2 mx-auto border rounded shadow" color="transparent"
-          name={props.value.userProfile.username} onClick={event => props.addFriend(event)}>Add</Button>
-      </Container> : null }
-    </Card>
+      <UserInfoCard value={props.value} />
+      {props.value.userProfile.username !== props.value.username ?
+        <Container className="py-2 text-center">
+          <Button className="mb-2 mx-auto border rounded shadow" color="transparent"
+            name={props.value.userProfile.username} onClick={event => props.addFriend(event)}>Add</Button>
+        </Container> : null}
+    </Card>)
+  } else {
+    return (<Card className="mx-1 mt-1 shadow">
+      <CardImg top className="border rounded shadow" width="100%" src={props.value.userProfile.avatar} alt="Profile Picture" />
+      <CardBody className='text-center border rounded shadow my-2 mx-2'>
+        <CardTitle><b>{props.value.userProfile.username}</b></CardTitle>
+        <CardSubtitle>-{props.value.userProfile.platform}-
+        </CardSubtitle>
+        <CardText>{props.value.userProfile.bio}</CardText>
+      </CardBody>
+      <UserInfoCard value={props.value} />
+    </Card>)
+  }
 
-  );
 }
 
 export default UserCard;
