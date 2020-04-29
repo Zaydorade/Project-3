e8d864c61ee060af4c9adc8fc44b1d388f3ccf3d
Zayd Sarraj <zayd.sarraj@gmail.com>
2020-04-29 00:16:21 -0400

    fixed a misspelled var

--- routes/api/users.js
+++ routes/api/users.js
@@ -93,7 +93,7 @@ router.route('/').post((req, res) => {
                         db.Users.create({
                             username: req.body.username,
                             password: hash,
-                            platform: platform,
+                            platform: newplatform,
                             bio: req.body.bio,
                             region: newRegion,
                             availability: req.body.availability,
